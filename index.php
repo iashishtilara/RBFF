61384b387b2eb9a4dd7df0cd628784f50fcdfc58
iashishtilara <101543876+iashishtilara@users.noreply.github.com>
2022-06-07 15:53:16 +0530

    as
    as

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+ <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="RSBRICKFIELD">
     <meta name="author" content="Devcrud">
-    <title>Creative Design | Free Bootstrap 4.3.x template</title>
+    <title>RBF</title>
 
     <!-- font icons -->
     <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
 
     <!-- Bootstrap + Creative Design main styles -->
 	<link rel="stylesheet" href="assets/css/creative-design.css">
+<style>
 
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+a {font-size: 200%;}
+center {font-size: 200%;}
+h6 {font-size: 200%;}
+</style>
 </head>
 <body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
     
@@ -32,8 +56,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#about">About</a>
                     </li>
+        
                     <li class="nav-item">
-                        <a class="nav-link" href="#features">Features</a>
+                        <a class="nav-link" href="#features">Pricing</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#contact">Contact</a>
@@ -47,8 +72,9 @@
     <header id="home" class="header">
         <div class="overlay"></div>
         <div class="header-content">
-            <h6>RBF BRICK FIELD</h6>
-            <h8>Premium Quality Bricks</h8> 
+            <h6 style="color:yellow;">RBF BRICK FIELD</h6>
+	    <h1>(राम सुचित ब्रिक फील्ड)</h1>
+            <h3>Premium Quality Bricks</h3> 
         </div>      
     </header><!-- End of Page Header -->    
     
@@ -64,14 +90,14 @@
                     <!-- row -->
                     <div class="row">
                         <div class="col">
-                            <p>"RBF Brick Field" is a well known name in Barabanki & need no introduction. it is established in 2001 but we are in this business since 30 years.So we regularly update ourself by research & development.</p>
+                            <p>"RBF Brick Field" is a well known name in Barabanki & need no introduction. It is established in 2001 but we are in this business since 30 years.</p>
                             <p><b></b></p>                        
-                            <p>We are equipped with highly productive equipment needed to minimize manufacturing costs while maintaining quality of the highest standards.we are constantly working towards more advanced development and state-of-the-art technology with the promise of an even brighter future, while providing a better service to our customers.</p>
+                            <p>We are equipped with highly productive equipment needed to minimize manufacturing costs while maintaining quality of the highest standards.</p>
                             <p></p>
                         </div>
                         <div class="col">
-                            <p>RBF brick field is now proven name in quality bricks. According to the market requirement and trends we have changed our manufacturing strategies and designs to suit the needs of time as well as the needs of our customers.</p>
-                            <p>Company makes use of best quality raw material and manufactures with the latest technology. </p>
+                            <p>RBF brick field is now proven name in quality bricks.Company makes use of best quality raw material and manufactures with the latest technology. </p>
+             
                             <p>For RBF Brick field, customers needs stand at the top ! we therefore, ensure them products with high strength, cost effectiveness, weather resistence and good thermal insulation.</p>
                         </div>
                     </div><!-- End of Row -->                   
@@ -88,7 +114,7 @@
     
 
     <!-- Features Section -->
-    <section class="has-bg-img" id="features">
+    <section class="has-bg-img">
         <div class="overlay"></div>
         <!-- Button trigger modal -->
         <a data-toggle="modal" href="#exampleModalCenter">
@@ -100,7 +126,7 @@
             <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                 <div class="modal-content">
                      <iframe width="100%" height="450px" class="border-0" 
-                    src="public_html\assets\imgs\New folder\WhatsApp Video 2022-06-04 at 4.35.41 PM.mp4">
+                    src="assets\imgs\New folder\WhatsApp Video 2022-06-04 at 4.35.41 PM.mp4">
                     </iframe>
                 </div>
             </div>
@@ -108,61 +134,114 @@
     </section><!-- End of features Section -->  
 
     <!-- Section -->
-    <section>
+    <section id="features">
         <!-- Container -->
         <div class="container">
             <!-- Row -->
             <div class="row justify-content-between align-items-center">
                 <div class="col-md-6">
-                    <div class="img-wrapper">
-                        <div class="after"></div>
-                        <img src="assets/imgs/img-3.jpg" class="w-100" alt="About us">
-                    </div>
+                    <div class="container">
+
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+	  <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="assets\imgs\avval.png" alt="Los Angeles" style="width:100%;">
+        <div class="carousel-caption">
+         
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="assets\imgs\lal peti.png" alt="Chicago" style="width:100%;">
+        <div class="carousel-caption">
+       
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="assets\imgs\tamda.png" alt="New York" style="width:100%;">
+        <div class="carousel-caption">
+     
+        </div>
+      </div>
+	  
+      <div class="item">
+        <img src="assets\imgs\peela.png" alt="New York" style="width:100%;">
+        <div class="carousel-caption">
+    
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
                 </div>
                 <div class="col-md-5">
-                    <h6 class="title mb-3">Lorem ipsum dolor sit amet, consectetur.</h6>
+                    <h6 class="title mb-3">Class-wise pricing of Bricks :-</h6>
                     
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae autem rem impedit molestiae hic ducimus, consequuntur ullam dolorem quaerat beatae labore explicabo, sint laboriosam aperiam nihil inventore facilis. Quasi, facilis.</p>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, amet!</p>
-                    <button class="btn btn-outline-primary btn-sm">Learn More</button>
+                    <table>
+  <tr>
+    <th><center>Class</center></th>
+    
+    <th><center>Price(in Rs./1000)</center></th>
+  </tr>
+  <tr>
+    <td><center>Class 1 (अव्वल )</td>
+    
+    <td><center>8000</td>
+  </tr>
+  <tr>
+    <td><center>Class 2 (लाल पेटी)</td>
+    
+    <td><center>7000</td>
+  </tr>
+  
+  <tr>
+    <td><center>Class 3 (तामड़ा)</td>
+    
+    <td><center>6500</td>
+<tr>
+    <td><center>Class 4 (पीला)</td>
+   
+    <td><center>4000</td>
+  </tr>
+  
+</table>
+                  
                 </div>
             </div><!-- End of Row -->           
         </div><!-- End of Container-->      
     </section><!-- End of Section -->   
+ <!-- Section -->
+  
 
     <!-- Testmonial Section -->
     
 
-    <!-- Contact Section -->
-    <section id="contact" class="text-center">
-        <!-- container -->
-        <div class="container">
-            <h1>CONTACT US</h1>
-            <p class="mb-5">If you have some Questions or need Help! Please Contact Us! <br> 
-            We make Cool and Clean Design for your Business</p>
-            
-            <!-- Contact form -->
-            <form class="contact-form col-md-11 col-lg-9 mx-auto">
-                <div class="form-row">
-                    <div class="col form-group">
-                        <input type="text" class="form-control" placeholder="Name">
-                    </div>
-                    <div class="col form-group">
-                        <input type="email" class="form-control" placeholder="Email">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <textarea name="" id="" cols="30" rows="5" class="form-control" placeholder="Your Message"></textarea>
-                </div>
-                <div class="form-group">
-                    <input type="submit" class="btn btn-primary btn-block" value="Send Message">
-                </div>
-            </form><!-- End of Contact form -->         
-        </div><!-- End of Container-->      
-    </section><!-- End of Contact Section -->   
+    
 
     <!-- Section -->
-    <section class="pb-0">
+    <section id="contact" class="pb-0">
         <!-- Container -->
         <div class="container">
             <!-- Pre footer -->
@@ -172,8 +251,8 @@
                         <h6 class="font-weight-bold">ABOUT US</h6>
                     </li>
                     <li class="list-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae nobis fugit maxime deleniti minus optio accusamus, quam maiores explicabo sunt.</p>
-                        <a href="#"><strong class="text-primary">Creative</strong> <span class="text-dark">Design</span></a>
+                        
+                        <a href="#"><strong class="text-primary">RBF</strong> <span class="text-dark">BRICKFIELD</span></a>
                     </li>
                 </ul>
                 <ul class="list">
@@ -181,9 +260,9 @@
                         <h6 class="font-weight-bold">CONTACT INFO</h6>
                     </li>
                     <li class="list-body">
-                        <p>Contact us and we'll get back to you within 24 hours.</p>
-                        <p><i class="ti-location-pin"></i> 12345 Fake ST NoWhere AB Country</p>
-                        <p><i class="ti-email"></i>  info@website.com</p>
+                        <p>Contact us at mobile number 9721666668 and we'll get back to you within 24 hours.</p>
+                        <p><i class="ti-location-pin"></i> Ram Suchit Brick Field, Village- Basiamau, Haidergarh Road Post-Sohawan, Barabanki</p>
+                        <p><i class="ti-email"></i>  manishsinghbbk400@gmail.com</p>
                         <div class="social-links">
                             <a href="javascript:void(0)" class="link"><i class="ti-facebook"></i></a>
                             <a href="javascript:void(0)" class="link"><i class="ti-twitter-alt"></i></a>
@@ -198,7 +277,7 @@
 
             <!-- foooter -->
             <footer class="footer">            
-                <p>Copyright <script>document.write(new Date().getFullYear())</script> &copy; <a href="http://www.devcrud.com">DevCRUD</a></p>
+                <p>Copyright <script>document.write(new Date().getFullYear())</script> &copy; RBF BRICKFIELD Barabanki</p>
             </footer><!-- End of Footer-->  
             
         </div><!--End of Container -->      
